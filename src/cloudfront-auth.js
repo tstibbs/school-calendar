4a878b7d57bade99fb3bb27baf59665b12f2b3e1
Tim Stibbs <tstibbs@users.noreply.github.com>
2026-03-11 21:00:10 +0000

    Tightening cloudfront auth and validation

--- src/cloudfront-auth.js
+++ src/cloudfront-auth.js
@@ -2,7 +2,7 @@ function handler(event) {
 	var request = event.request
 	var headers = request.headers
 
-	// This placeholder will be replaced by the CDK during deployment
+	// This placeholder will be replaced by CDK during deployment
 	var AUTH_PASSWORD = '%%AUTH_PASSWORD%%'
 
 	if (!headers['x-auth-password'] || headers['x-auth-password'].value !== AUTH_PASSWORD) {
@@ -12,6 +12,19 @@ function handler(event) {
 			body: {encoding: 'text', data: 'Unauthorized: Invalid Password'}
 		}
 	}
-
+	if (request.method != 'PUT') {
+		return {
+			statusCode: 403,
+			statusDescription: 'Forbidden',
+			body: {encoding: 'text', data: 'Unauthorized: Disallowed method'}
+		}
+	}
+	if (!/^\/[\w-_]+\/input\.pdf$/.test(request.uri)) {
+		return {
+			statusCode: 403,
+			statusDescription: 'Forbidden',
+			body: {encoding: 'text', data: 'Unauthorized: Invalid file path'}
+		}
+	}
 	return request
 }
